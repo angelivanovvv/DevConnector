69f15f83a01c8c51cbc841df01b56a8e97b9767c
Angel Ivanov <a_ivanovvv@abv.bg>
2020-01-22 20:09:21 +0200

    prepared application for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoDB = require("./config/db");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -13,21 +14,27 @@ const postsRoute = require("./routes/posts");
 //Connect mongoDB DataBase
 mongoDB();
 
-//Enable CORS
-app.use(cors({ origin: "http://localhost:3000" }));
+//Enable all CORS
+app.use(cors());
 
 //Init Middleware
 app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
 
 //Define Routes - Connect routes to server
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/posts", postsRoute);
+
+//Serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
