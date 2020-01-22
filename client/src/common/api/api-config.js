e2f07c9bd5d2540c20ca42d7127462e392fb5526
Angel Ivanov <a_ivanovvv@abv.bg>
2020-01-22 20:50:08 +0200

    fixed clinet to connect prod 2

--- client/src/common/api/api-config.js
+++ client/src/common/api/api-config.js
@@ -1,11 +1,12 @@
 export const settings = {
   protocol: "http://",
+  prodProtocol: "https://",
   basePath: "localhost:5000",
   prodBasePath: "whispering-tor-02190.herokuapp.com"
 };
 
 export const config = Object.freeze({
-  baseURL: `${settings.protocol}${settings.prodBasePath}`,
+  baseURL: `${settings.prodProtocol}${settings.prodBasePath}`,
   headers: {
     "Content-Type": "application/json"
   }
