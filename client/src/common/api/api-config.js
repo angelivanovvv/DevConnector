69aea6487b0b6a05a3b6a8e918bae0cb9e15173d
Angel Ivanov <a_ivanovvv@abv.bg>
2020-01-22 20:44:34 +0200

    fixed client to connect prod

--- client/src/common/api/api-config.js
+++ client/src/common/api/api-config.js
@@ -1,10 +1,11 @@
 export const settings = {
   protocol: "http://",
-  basePath: "localhost:5000"
+  basePath: "localhost:5000",
+  prodBasePath: "whispering-tor-02190.herokuapp.com"
 };
 
 export const config = Object.freeze({
-  baseURL: `${settings.protocol}${settings.basePath}`,
+  baseURL: `${settings.protocol}${settings.prodBasePath}`,
   headers: {
     "Content-Type": "application/json"
   }
--- client/src/common/api/axios.js
+++ client/src/common/api/axios.js
@@ -1,29 +1,9 @@
 import Axios from "axios";
 import { config } from "./api-config";
-// import { createBrowserHistory } from "history";
-
-// import * as authActions from "../../reduxSources/actions/authActions";
-// import { ROUTES_ACTIONS } from "../../constants/clientRoutes";
-
-// import initStore from "../../reduxSources/store";
-
-// const history = createBrowserHistory();
-// const store = initStore(history);
 
 const instance = Axios.create({
   baseURL: config.baseURL,
   headers: config.headers
 });
-
-// instance.interceptors.response.use(
-//   response => response,
-//   error => {
-//     if (error.response.status === 401) {
-//       store.dispatch(authActions.unAutorized());
-//       store.dispatch(ROUTES_ACTIONS.toLogout());
-//     }
-//     return Promise.reject(error);
-//   }
-// );
 
 export default instance;
